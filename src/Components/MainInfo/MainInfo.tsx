882e5fad78b97f517e137bdfc4b37d12561a88aa
ZhannaL <zhannet1814@gmail.com>
2023-10-11 23:57:23 +0200

    updatelayout

--- src/Components/MainInfo/MainInfo.tsx
+++ src/Components/MainInfo/MainInfo.tsx
@@ -7,18 +7,27 @@ import foto from '../../images/foto.jpg';
 export const MainInfo = () => {
   const isMobile = useMediaQuery<Theme>((theme) => theme.breakpoints.down('sm'));
 
-  return (
+  return isMobile ? (
+    <Box display="flex" flexDirection="column">
+      <Box display="flex" justifyContent="space-between" width="100%">
+        <NameAndTitle />
+        <img
+          style={{ width: 'auto', height: '110px', marginRight: '20px' }}
+          src={foto}
+          alt="profileFoto"
+        />
+      </Box>
+
+      <ContactDetails />
+    </Box>
+  ) : (
     <Box display="flex" justifyContent="space-between" width="100%">
       <Box display="flex" flexDirection="column">
         <NameAndTitle />
         <ContactDetails />
       </Box>
       <Box display="flex" alignItems={isMobile ? 'flex-start' : 'center'}>
-        <img
-          style={{ width: 'auto', height: isMobile ? '100px' : '205px' }}
-          src={foto}
-          alt="profileFoto"
-        />
+        <img style={{ width: 'auto', height: '205px' }} src={foto} alt="profileFoto" />
       </Box>
     </Box>
   );
--- src/Components/MainInfo/MainInfoItems/NameAndTitle.tsx
+++ src/Components/MainInfo/MainInfoItems/NameAndTitle.tsx
@@ -1,9 +1,11 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Theme, Typography, useMediaQuery } from '@mui/material';
 
 export const NameAndTitle = () => {
+  const isMobile = useMediaQuery<Theme>((theme) => theme.breakpoints.down('sm'));
+
   return (
     <Box display="flex" flexDirection="column" alignItems="flex-start" py={3}>
-      <Typography variant="h4" fontWeight={700}>
+      <Typography variant={isMobile ? 'h5' : 'h4'} fontWeight={700}>
         Zhanna Sbitneva
       </Typography>
       <Typography color="primary" variant="h6">
